bf2f5aca9d774f61f84d92f73e9db9a208d5bd2f
Amritpal <amrit66266@gmail.com>
2022-12-13 13:00:47 +0530

    login update

---
+++ utils/StorageHandling.js
@@ -0,0 +1,24 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { ASYNC_KEY } from './string';
+
+export async function setItem(item, value) {
+  await AsyncStorage.setItem(item, value);
+}
+export async function getItem(item) {
+  let result;
+  await AsyncStorage.getItem(item, (err, value) => {
+    if (err) throw new Error(err);
+    else result = value;
+  });
+  return result;
+}
+export async function multiRemove(keys) {
+  AsyncStorage.multiRemove(keys, err => {
+    console.log('Stuff Removed');
+    getAllKeys();
+  });
+}
+export async function getAllKeys() {
+  const keys= await AsyncStorage.getAllKeys();
+  console.log(keys);
+}
---
+++ utils/string.js
@@ -0,0 +1,336 @@
+export const MANAGE_SCREEN = {
+    postPaidAccount: 'Postpaid Account',
+    payBillBtn: 'Pay Bill',
+    yourBill: 'Your Bill',
+    dueIn: 'Due In',
+    mngAccount: 'Manage Account',
+    prePaidAcount: 'Prepaid Account',
+    rechargeBtn: 'Recharge',
+    balance: 'Balance',
+    expDate: 'Expiry Date',
+    addNewAccount: 'Add New Account',
+    myCards: 'My Cards',
+    payBill: 'Pay Bills',
+    prePaidBill: 'Recharge',
+    transHistory: 'Transaction History',
+    USSDCodeScreen: 'USSD Code Screen',
+    USSDCode: 'Quick Codes',
+    serviceStoreLoc: 'Service Store Locator',
+    getBillDetails: 'Refresh',
+    getBalanceDetails: 'Refresh',
+  };
+  
+  export const RECHARGE_SCREEN = {
+    title: 'Recharge',
+    prepaid: 'Prepaid',
+    dth: 'DTH',
+    broadband: 'Broadband',
+  };
+  export const ALERT_BOX_TYPE = {
+    error: 'error',
+    paymentConfirmation: 'payConfirmation',
+    confirmation: 'confirmation',
+  };
+  export const ALERT_CONTENT = {
+    error: 'Error',
+    confirmation: 'Confirmation',
+    somethingWentWrong: 'Something went wrong !!!',
+    alert: 'Alert',
+  };
+  export const BILL_PAY_SCREEN = {
+    title: 'Bill Pay',
+    enterAccNo: 'Enter Account Number',
+    enterAmount: 'Enter Amount',
+    yourAccounts: 'Your Account',
+    selectCard: 'Select Card to Pay',
+    selected: 'Selected',
+    proceed: 'Proceed',
+    confirmation: 'Confirmation',
+    enterMobileNumber: 'Mobile Number',
+    billPayment: 'Bill Payment',
+    accountHolderName: 'Account Holder Name',
+    accountNumber: 'Account Number',
+    ACCOUNT_TYPE: 'Account Type',
+    YOUR_BILL: 'Due Amount',
+    lastBillAmount: 'Previous Balance',
+    currentOutStanding: 'Total Due',
+    amount: 'Amount',
+    proceedToPay: 'Proceed To Pay',
+    flow: 'Flow',
+  };
+  export const ASYNC_KEY = {
+    token: 'TOKEN',
+    auth: 'AUTH',
+    NotificationFlag: 'Flag',
+    LOOKUP: 'Lookup key',
+    WELCOME_SCREEN_VISITED: 'Welcome Screen',
+    SELECTED_THEME:'Selected Theme',
+    LOGGEDIN:'is Logged in',
+    LOGGEDIN_CREDS:'LoginCredentials',
+    ACCOUNTS:"Accounts",
+    USER_PROFILE_DATA:"UserProfileData",
+    Promo_Api_Response:'PromoApiResponse',
+    MOBILE:"Mobile",
+    MOBILE_VISIBLE:"MobileVisible",
+    googleIdToken:'GoogleIdToken',
+    loginMethod:'Login Method',
+    facebookIdToken:'fbToken',
+  };
+  export const MY_CARDS_SCREEN = {
+    title: 'My Cards',
+  };
+  export const STORE_LOCATOR_SCREEN = {
+    title: 'Find A Store',
+    inputText: 'Select Category',
+    locationInput: 'Enter Location',
+    storeLocatorUrl:'storeLocatorUrl',
+  };
+  
+  export const YOUR_ACCOUNTS_SCREEN = {
+    title: 'Your Account',
+    postPaidAccount: 'Postpaid Account',
+    payBillBtn: 'Pay Bill',
+    yourBill: 'Your Bill',
+    dueIn: 'Due In',
+    mngAccount: 'Manage Account',
+    prePaidAcount: 'Prepaid Account',
+    rechargeBtn: 'Recharge',
+    balance: 'Balance',
+    expDate: 'Expiry Date',
+    verifyAccount: 'Verify Account',
+    desc: 'Please enter the OTP you have received on your registered mobile number.',
+    type: 'Type',
+    mobileNum: 'Mobile Number',
+    proceed: 'Proceed',
+    enterOtp: 'Enter OTP',
+    cancel: 'Cancel',
+    verify: 'Verify',
+    addAccount: 'Add Account',
+    errorMsg: 'Error',
+    getBillDetails: 'Refresh',
+    getBalanceDetails: 'Refresh',
+  };
+  
+  export const SPLASH_SCREEN = {
+    title: 'Welcome To bMobile',
+    next: 'Next',
+    signUp: 'Sign Up',
+    SUCCESS: 'Success',
+    ERROR: 'Error',
+    INFO: 'info',
+  };
+  export const SIGNUP_SCREEN = {
+    title: 'Sign Up',
+    next: 'Submit',
+    previous: 'Previous',
+    firstname: 'First Name',
+    lastname: 'Last Name',
+    username: 'Username',
+    phone: 'Mobile Number',
+    email: 'Email Address',
+    password: 'Password',
+    confirmpassword: 'Confirm Password',
+    infoPassword:
+      '\u2B24 Minimum eight and maximum 20 characters.\n\u2B24 Atleast one uppercase letter.\n\u2B24 Atleast one lowercase letter.\n\u2B24 Atleast one number and one special character.',
+  };
+  export const LOGIN_SCREEN = {
+    title: 'Welcome To bMobile',
+    login: 'Sign In',
+    previous: 'Previous',
+    username: 'Mobile Number',
+    password: 'Password',
+    google: 'Sign in with Google',
+    facebook: 'Continue with Facebook',
+  };
+  export const MANAGE_ACCOUNT = {
+    title: 'Your Account',
+    accountHolderName: 'Name',
+    accountNumber: 'Account Number',
+    balance: 'Balance',
+    expDate: 'Expiry Date',
+    dueAmount: 'Due Amount',
+    dueDate: 'Due Date',
+    accountType: 'Account Type',
+    status: 'Status',
+    deleteAcc: 'Delete Account',
+    mobile: 'Mobile Number',
+  };
+  export const PROMOTIONS_SCREEN = {
+    title: 'Promotions',
+    image: 'Image',
+    bannerImages: 'BannerImage',
+  };
+  export const VERIFYOTP_SCREEN = {
+    title1: 'Sign Up - Verify OTP',
+    title2: 'Verify OTP',
+    next: 'Verify',
+    previous: 'Previous',
+    otp: 'OTP',
+    continue: 'Verify',
+    cancel: 'Cancel',
+  };
+  export const FORGET_PASSWORD_SCREEN = {
+    title: 'Forgot Password',
+    continue: 'Continue',
+    previous: 'Previous',
+    Email_Mobile: 'Mobile Number',
+  };
+  
+  export const NEW_PASSWORD_SCREEN = {
+    title: 'New Password',
+    title2: 'Reset Password',
+    submit: 'Submit',
+    update: 'Update',
+    cancel: 'Cancel',
+    newpassword: 'New Password',
+    confirmpassword: 'Confirm Password',
+  };
+  export const TERMS_CONDITIONS = {
+    title: 'Terms & Conditions',
+    agree: 'Agree & Proceed',
+    previous: 'Cancel',
+  };
+  export const PAASWORD_CHANGED_SCREEN = {
+    title: 'Password Changed Successfully!',
+    login: 'Log In',
+  };
+  
+  export const RECHARGE_FLOW_SCREEN = {
+    name: 'Mobile Number',
+    title: 'Recharge',
+    operator: 'Select Operator',
+    proceed: 'Proceed',
+    search: 'Search',
+  };
+  export const HOME_SCREEN = {};
+  export const SCREEN_ROUTE_MAPPING = {
+    RechargeSelectPlan: 'RechargeSelectPlan',
+    PaymentSuccessScreen: 'PaymentSuccessScreen',
+    Home: 'Home',
+    WelcomeScreen: 'WelcomeScreen',
+    SignUpScreen: 'SignUpScreen',
+    VerifyOtpScreen: 'VerifyOtpScreen',
+    TermsConditionsScreen: 'TermsConditionsScreen',
+    LoginScreen: 'LoginScreen',
+    ForgetPasswordScreen: 'ForgetPasswordScreen',
+    NewPasswordScreen: 'NewPasswordScreen',
+    TransactionHistory: 'TransactionHistory',
+    TransactionStatus: 'TransactionStatus',
+    DrawerContent: 'DrawerContent',
+    InitialApiHitScreen: 'InitialApiHitScreen',
+  };
+  export const RECHARGE_SELECT_PLAN = {
+    recharge: 'Recharge - Select Plan',
+    proceed: 'Proceed To Pay',
+    plan: 'Plans',
+    topup: 'Top UP',
+  };
+  export const Payment_Success_Screen = {
+    paymentSuccess: 'Payment Successful',
+    paymentFailed: 'Payment Failed',
+    home: 'Home',
+  };
+  export const Alert_Dialog = {
+    selectCard: 'Select Card to Pay',
+    cvv: 'CVV',
+    cancel: 'Cancel',
+    confirm: 'Confirm',
+    addNewCard: 'Add New Card',
+    cvv: 'CVV',
+    cardNumber: 'Card Number',
+    mmyy: 'MM/YY',
+  };
+  
+  export const ADD_CARD_ALERT = {
+    selectCard: 'Select Card to Pay',
+    cvv: 'CVV',
+    cancel: 'Cancel',
+    confirm: 'Confirm',
+    addNewCard: 'Add New Card',
+    cvv: 'CVV',
+    cardNumber: 'Card Number',
+    mmyy: 'MM/YY',
+    CARDS: 'Cards',
+  };
+  
+  export const Confirm_Screen = {
+    confirm: 'Please Confirm',
+    topUpConfirm: 'Are you sure you want top up ',
+    billPayConfirm: 'Are you sure you want Bill Pay on ',
+    selectPlan: 'Are you sure you want to apply bundle on ',
+  };
+  export const Support_Screen = {
+    title: 'Help and Support',
+    submit: 'Submit',
+    subject: 'Subject',
+    firstName: 'First Name',
+    lastName: 'Last Name',
+    phoneNumber: 'Mobile Number',
+    provideDescription: 'Provide Description',
+  };
+  
+  export const ACCOUNT_ADDED_SCREEN = {
+    accAdded: 'Account Added',
+    home: 'Home',
+    firstLine:
+      'Your Account has been added to your profile and you can view or edit it in',
+  };
+  
+  export const DRAWER_CONTENT = {
+    payBill: 'Pay Bills',
+    prepaid: 'Recharge',
+    serviceStoreLoc: 'Service Store Locator',
+    transHistory: 'Transaction History',
+    switch: 'Switch Theme',
+    signOut: 'Sign Out',
+    name: '',
+    num: '',
+    editProfile: 'Edit Profile',
+    names: 'NAME',
+    mobileNumber: 'MOBILE_NUMBER',
+    type: 'TYPE',
+    accountNumber: 'Account_NUMBER',
+    amount: 'AMOUNT',
+    manageAccount: 'Manage Account',
+  };
+  
+  export const TRANSACTION_HISTORY = {
+    txnHistory: 'transactionHistory',
+  };
+  export const TRANSACTION_STATUS = {
+    back: 'Back',
+    NOTE:"**Transactions may be subjected to additional Bank Charges"
+  };
+  export const INVALID_INPUT = {
+    username: 'Username',
+    mobileNumber: 'Mobile Number',
+    password: `Password is required`,
+    currPassword: `Current Password is required`,
+    passwordField: `Password`,
+    otp: 'Please enter a valid 6-Digit OTP',
+    firstName: 'First Name',
+    lastname: 'Last Name',
+    phone: 'Phone',
+    phoneNumber: 'Phone number',
+  
+    email: 'Email Address',
+    description: 'Provide Description',
+    emptyText: ` can't be empty`,
+    requiredText: ` is required`,
+    confirmpassword: `Confirm Password is required`,
+    confirmpasswordmessage: `Passwords don't match`,
+    subject: 'Subject',
+    contact: 'Account Number',
+    amount: 'Amount',
+    mobileNumber: 'Mobile Number',
+    voucher: 'Voucher',
+    newpassword: `New Password is required`,
+  };
+  
+  export const BUTTON_TYPE = {
+    flatGreenButton: 'Flat Green Button',
+  };
+  export const BuildType = {
+    BUILD: 'DEV - ',
+  };
+  
